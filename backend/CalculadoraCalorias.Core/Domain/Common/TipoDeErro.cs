cffab9f7e2bd9dd5ab00d459ca88ab5d3521f051
Marcel Zimmer <Marcel-zimmer>
2026-02-02 20:29:01 -0300

    feat(api):adicionado metodo generico para responder requisicoes com o status code correto

---
+++ backend/CalculadoraCalorias.Core/Domain/Common/TipoDeErro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraCalorias.Core.Domain.Common
+{
+    public enum TipoDeErro
+    {
+        None,
+        NotFound,
+        Validation,
+        Conflict,    
+        Unauthorized,
+        SystemFailure 
+    }
+}
