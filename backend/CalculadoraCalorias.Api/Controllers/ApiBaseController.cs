e59ad14b342c6dd95f64f8de4e560f1c97f266e7
Marcel Zimmer <marcel-zimmer@hotmail.com>
2026-02-08 02:14:40 -0300

    feat(api):adicionado metodo generico para responder requisicoes com o status code correto

---
+++ backend/CalculadoraCalorias.Api/Controllers/ApiBaseController.cs
@@ -0,0 +1,40 @@
+﻿using CalculadoraCalorias.Core.Domain.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraCalorias.Api.Controllers
+{
+    public class ApiBaseController : ControllerBase
+    {
+        protected IActionResult ProcessarResultado<T>(Resultado<T> result)
+        {
+            if (result.Sucesso)
+            {
+                return Ok(result.Data);
+            }
+
+            return ConverterErroParaHttp(result);
+        }
+
+        protected IActionResult ProcessarResultado(Resultado result)
+        {
+            if (result.Sucesso)
+            {
+                return NoContent();
+            }
+
+            return ConverterErroParaHttp(result);
+        }
+
+        private IActionResult ConverterErroParaHttp(Resultado result)
+        {
+            return result.TipoErro switch
+            {
+                TipoDeErro.NotFound => NotFound(result.MensagemErro),
+                TipoDeErro.Validation => BadRequest(result.MensagemErro),
+                TipoDeErro.Unauthorized => Unauthorized(result.MensagemErro),
+                TipoDeErro.Conflict => Conflict(result.MensagemErro),
+                _ => StatusCode(500, "Erro desconhecido")
+            };
+        }
+    }
+}
---
+++ backend/CalculadoraCalorias.Core/Domain/Common/Resultado.cs
@@ -0,0 +1,58 @@
+﻿
+
+namespace CalculadoraCalorias.Core.Domain.Common
+{
+    public class Resultado
+    {
+        public bool Sucesso { get; }
+        public TipoDeErro TipoErro { get; }
+        public string? MensagemErro { get; }
+
+        protected Resultado(bool sucesso, TipoDeErro tipoErro, string mensagemErro)
+        {
+            Sucesso = sucesso;
+            TipoErro = tipoErro;
+            MensagemErro = mensagemErro;
+        }
+        protected Resultado(bool sucesso)
+        {
+            Sucesso = sucesso;
+
+        }
+
+        public static Resultado Success()
+        {
+            return new Resultado(true);
+        }
+
+        public static Resultado Failure(TipoDeErro tipoErro, string mensagemErro)
+        {
+            return new Resultado(false, tipoErro, mensagemErro);
+        }
+    }
+    public class Resultado<T> : Resultado
+    {
+        public T? Data { get; }
+        private Resultado(T? data, bool sucesso, TipoDeErro tipoErro, string mensagemErro)
+            : base(sucesso, tipoErro, mensagemErro)
+        {
+            Data = data;
+        }
+        private Resultado(T? data, bool sucesso)
+            : base(sucesso)
+        {
+            Data = data;
+        }
+        public static Resultado<T> Success(T data)
+        {
+            return new Resultado<T>(data, true, TipoDeErro.None, string.Empty);
+        }
+
+        public new static Resultado<T> Failure(TipoDeErro tipoErro, string mensagemErro)
+        {
+            return new Resultado<T>(default, false, tipoErro, mensagemErro);
+        }
+
+        public static implicit operator Resultado<T>(T data) => Success(data);
+    }
+}
